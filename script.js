c482920aaebf4c04bc52243a6a36958e6e0afe67
mohammadyusuf8868-art <mohammadyusuf8868@gmail.com>
2026-08-09 19:53:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ window.addEventListener("load", () => {
 
 /*for sound effect*/
 
+const sound = document.getElementById("welcomeSound");
+
+window.addEventListener("scroll", () => {
+    sound.play();
+}, { once: true });
+
 document.addEventListener("pointerdown", () => {
     sound.play();
 }, { once: true });
